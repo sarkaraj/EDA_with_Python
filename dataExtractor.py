0b4114ba5d99e3c17961c0cc2e5d8726d66c2c03
Rajarshi <rajarshisarkar21@gmail.com>
2017-03-30 17:20:57 +0530

    Data Extractor Completed

---
+++ dataExtractor.py
@@ -0,0 +1,25 @@
+import itertools
+import xml.etree.ElementTree as xmlread
+
+import pandas as pd
+
+
+def getDataFrameFromXml(xmlAddress):
+    df = pd.DataFrame()
+    file = xmlread.parse(xmlAddress).getroot()
+
+    for child_nodes in file:
+        child = child_nodes.attrib
+        dictKeys = child.keys()
+        noOfKeys = len(dictKeys)
+        keyArray = [0] * noOfKeys
+        for i in range(0, noOfKeys, 1):
+            try:
+                keyArray[i] = child[dictKeys[i]]
+            except KeyError:
+                continue
+
+        kVDict = dict(itertools.izip_longest(dictKeys, keyArray))
+        df = df.append(kVDict, ignore_index=True)
+
+    return df
---
+++ votesEDA.py
@@ -0,0 +1,11 @@
+import dataExtractor
+
+dataFile = "./ai.stackexchange.com/Votes.xml"
+
+df = dataExtractor.getDataFrameFromXml(dataFile)
+
+# temp_df = df.sample(frac=0.01)
+#
+# print temp_df.describe
+
+print df.describe(include='all')
